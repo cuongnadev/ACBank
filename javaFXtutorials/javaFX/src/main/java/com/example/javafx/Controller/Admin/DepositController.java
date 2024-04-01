656900b5c4b3a431fa144ac1bcfe7f2065d46305
Cậu Bé Java <154894942+cuongnadev@users.noreply.github.com>
2024-04-01 14:20:54 +0700

    new update

--- javaFXtutorials/javaFX/src/main/java/com/example/javafx/Controller/Admin/DepositController.java
+++ javaFXtutorials/javaFX/src/main/java/com/example/javafx/Controller/Admin/DepositController.java
@@ -3,31 +3,127 @@ package com.example.javafx.Controller.Admin;
 import com.example.javafx.Models.*;
 import com.example.javafx.Controller.GetView.CheckingCellFactory;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Optional;
+import java.util.Random;
 import java.util.ResourceBundle;
 
 public class DepositController implements Initializable {
     public TextField pAddress_fld;
     public Button search_btn;
     public ListView<CheckingAccount> result_listview;
-    public TextField withdrawal_lbl;
     public Button deposit_btn;
     public Button withdrawal_btn;
-    public TextField deposit_lbl;
+    public TextField deposit_tfd;
+    public TextField withdrawal_tfd;
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         search_btn.setOnAction(event -> onSearch());
+        deposit_btn.setOnAction(event -> onDeposit());
+        withdrawal_btn.setOnAction(event -> onWithdrawal());
         refreshData();
         Model.getInstance().getViewFactory().setDepositController(this);
+    }
+
+    private void onWithdrawal() {
+        ResultSet resultSet = Model.getInstance().getDatabaseDriver().getChekingAccountsData();
+        String payeeAddress = pAddress_fld.getText().trim();
+        Double amount = 0.0;
+        try {
+            amount = Double.parseDouble(withdrawal_tfd.getText().trim());
+        } catch (NumberFormatException e) {
+            showAlertError("Invalid amount. Please enter a valid number.");
+            return;
+        }
+        // Kiểm tra tài khoản
+        if (payeeAddress.isEmpty() ){
+            showAlertError("Error! Please select the account you want to deposit.");
+            return;
+        }
+        // kiểm tra số tiền
+        if (amount <= 0){
+            showAlertError("Please enter valid Amount.");
+        }
+        try {
+            while (resultSet.next()){
+                if (resultSet.getString("Owner").equals(payeeAddress)){
+                    double amountCH = Double.valueOf(resultSet.getString("Balance"));
+
+                    if(amountCH < amount){
+                        showAlertError("Insufficient funds. Please check your balance.");
+                        return;
+                    }
+                    Model.getInstance().getDatabaseDriver().updateAccountBalance(payeeAddress, amountCH - amount);
+                    showAlert("Withdrawal Successful!");
+                    refreshData();
+                    return;
+                }
+            }
+            showAlertError("Error! Enter payee address no valid.");
+            refreshData();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void onDeposit() {
+        ResultSet resultSet = Model.getInstance().getDatabaseDriver().getChekingAccountsData();
+        String payeeAddress = pAddress_fld.getText().trim();
+        Double amount = 0.0;
+        try {
+            amount = Double.parseDouble(deposit_tfd.getText().trim());
+        } catch (NumberFormatException e) {
+            showAlertError("Invalid amount. Please enter a valid number.");
+            return;
+        }
+        // Kiểm tra tài khoản
+        if (payeeAddress.isEmpty() ){
+            showAlertError("Error! Please select the account you want to deposit.");
+            return;
+        }
+
+        // kiểm tra số tiền
+        if (amount <= 0){
+            showAlertError("Please enter valid Amount.");
+            return;
+        }
+        if (amount >= 2100000) {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Confirmation Message");
+            alert.setHeaderText(null);
+            alert.setContentText("Would you like to create a savings account ?");
+            Optional<ButtonType> option = alert.showAndWait();
+            if (option.get().equals(ButtonType.OK)) {
+                //Tạo account Saving
+                String SavingNumber = "3021 " + RanDomNumber();
+                SavingAccount savingAccount = new SavingAccount(payeeAddress, SavingNumber, amount, 2000);
+                Model.getInstance().getDatabaseDriver().insertSavingAccount(savingAccount);
+                return;
+            }
+        }
+
+        try {
+            while (resultSet.next()){
+                if (resultSet.getString("Owner").equals(payeeAddress)){
+                    double amountCH = Double.valueOf(resultSet.getString("Balance"));
+                    Model.getInstance().getDatabaseDriver().updateAccountBalance(payeeAddress, amountCH + amount);
+                    showAlert("Deposit Successful!");
+                    refreshData();
+                    return;
+                }
+            }
+            showAlertError("Error! Enter payee address no valid.");
+            refreshData();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 
     public void onSearch(){
@@ -50,7 +146,7 @@ public class DepositController implements Initializable {
                 }
             }
             if (check == false){
-                showAlert("Error! Enter payee address no valid.");
+                showAlertError("Error! Enter payee address no valid.");
                 refreshData();
             }
         } catch (SQLException e) {
@@ -59,8 +155,17 @@ public class DepositController implements Initializable {
     }
 
 
-    private void showAlert(String message) {
+
+
+
+    private void showAlertError(String message) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+    private void showAlert(String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setHeaderText(null);
         alert.setContentText(message);
         alert.showAndWait();
@@ -68,6 +173,13 @@ public class DepositController implements Initializable {
 
     public void refreshData(){
         pAddress_fld.setText("");
+        deposit_tfd.setText("");
+        withdrawal_tfd.setText("");
         result_listview.getItems().clear();
+    }
+
+    public int RanDomNumber(){
+        Random random = new Random();
+        return 1000 + random.nextInt(9000);
     }
 }
